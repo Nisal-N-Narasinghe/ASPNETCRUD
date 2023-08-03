dccf54fa312799beadaaaef3ce17dd3e106b6d07
Nisal <it21259302@my.sliit.lk>
2023-08-03 12:36:27 +0530

    CRUD - ADD part completed

---
+++ CRUD/CRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using CRUD.Data;
+using CRUD.Models;
+using CRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class EmployeeController : Controller
+
+       
+    {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        public EmployeeController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+            };
+
+            await mvcDemoDbContext.Employees.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+        }
+
+        
+
+    }
+}
