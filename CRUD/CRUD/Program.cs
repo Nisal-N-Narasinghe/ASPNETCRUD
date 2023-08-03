39daed6712898ad1562045c38af9aab4b36bc131
Nisal <it21259302@my.sliit.lk>
2023-08-03 11:42:17 +0530

    db connction

--- CRUD/CRUD/Program.cs
+++ CRUD/CRUD/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CRUD.Data.MVCDemoDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("MvcDemoConnectionString")));
 
 var app = builder.Build();
 
